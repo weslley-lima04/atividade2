d28327789b86d08268b54f3a023d5d852bb42762
weslley-lima24 <weslley.lima24@etec.sp.gov.br>
2022-04-06 00:31:45 -0300

    Fizemos as 3 telas e adicionamos ícones

---
+++ app/src/main/java/br/com/etecia/atividade2/inicio.java
@@ -0,0 +1,14 @@
+package br.com.etecia.atividade2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class inicio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inicio_layout);
+    }
+}
